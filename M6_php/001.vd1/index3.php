0efd85eccddf534d0cd599187933a537069c73cd
BVCN 88 <manhcuong97kma@gmail.com>
2019-06-28 19:26:52 -0700

    1

---
+++ M6_php/001.vd1/index3.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+</head>
+<body>
+<pre>
+   nhúng cú pháp php vào trong code html
+    nhưng phải nhớ rằng đuôi file luôn là .php
+    cú pháp <?php ?>
+    -lệnh echo trong php là lệnh xuất ra màn hình
+    -các câu lệnh kết thúc bằng ;
+    -echo thẻ html
+</pre>
+
+<?php
+echo "chào Mạnh Cường";
+//echo trực tiếp ra thẻ html
+echo "<p style='color: yellow'>Chào php</p>";
+?>
+
+<pre>
+    cách số 2 chỉ in ra chuỗi trong php còn thẻ html thì để bên ngoài
+
+</pre>
+
+<p style='color: red'><?php "Chào php"; ?></p>
+</body>
+</html>
